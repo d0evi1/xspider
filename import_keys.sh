12f330ccad3d723305a86ac009e1921a05c3557c
d0evi1 <3331000@qq.com>
2014-12-05 11:24:18 +0800

    add some update commands.

--- import_keys.sh
+++ import_keys.sh
@@ -7,14 +7,40 @@
 
 
 
-function main()
+function add_all()
 {
     rm ./output.txt
-    echo "select subject_id from t_movies" | mysql -uroot douban_movie -N > ./ids.txt
-    while read line
-    do
-        echo "sadd xspider.set.MovieSpider $line " | redis-cli >> output.txt
-    done < ./ids.txt 
+    echo "select subject_id from t_movies" | mysql -uroot douban_movie -N |
+        while read line
+        do
+            echo "sadd xspider.set.MovieSpider $line " | redis-cli >> output.txt
+        done 
 }
 
-main
+function add_update()
+{
+    input=$1
+    rm ./output.txt
+    echo "select subject_id from t_movies where update_time > $input" | mysql -uroot douban_movie -N |
+        while read line
+        do
+            echo "sadd xspider.set.MovieSpider $line " | redis-cli >> output.txt
+        done 
+}
+
+case $1 in
+all)
+    echo "add_all"
+    add_all
+    ;;
+
+update)
+    echo "add_update"
+    add_update $2
+    ;;
+
+*)
+    echo "usage: $0 {all | update}"
+esac
+
+exit 0
